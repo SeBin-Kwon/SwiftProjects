33537a162c360a5b97c4fae32e28e521f1b8f2a0
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-03 16:33:05 +0900

    테이블뷰컨트롤러 커스텀셀 구성

---
+++ SecondWeek/UserTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserTableViewCell.swift
+//  SecondWeek
+//
+//  Created by Sebin Kwon on 1/3/25.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var messageLabel: UILabel!
+}
