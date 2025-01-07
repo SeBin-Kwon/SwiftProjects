933a814f04dbc8215c8b45887d526ef6c9e1bd04
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-07 11:16:50 +0900

    화면전환

---
+++ SecondWeek/GrayViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GrayViewController.swift
+//  SecondWeek
+//
+//  Created by Sebin Kwon on 1/7/25.
+//
+
+import UIKit
+
+class GrayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func popButtonTapped(_ sender: UIButton) {
+        print(#function)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func dismissButtonTapped(_ sender: UIButton) {
+        print(#function)
+        dismiss(animated: true)
+    }
+    
+    
+}
