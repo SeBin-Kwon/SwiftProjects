b95b285483cb741a83077d27c850f031676b655b
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-07 18:05:12 +0900

    CollectionView

---
+++ SecondWeek/Ten/TenCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TenCollectionViewCell.swift
+//  SecondWeek
+//
+//  Created by Sebin Kwon on 1/7/25.
+//
+
+import UIKit
+
+class TenCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        posterImageView.backgroundColor = .brown
+        posterImageView.layer.cornerRadius = 8
+        posterImageView.clipsToBounds = true
+    }
+
+}
