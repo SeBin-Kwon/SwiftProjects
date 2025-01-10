43328d3232bbf083cbbc83e0b97b05ce06b954a8
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-10 15:21:06 +0900

    init TravelTalk Project

---
+++ TravelTalk/TravelTalk/TravelTalkViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk
+//
+//  Created by Sebin Kwon on 1/10/25.
+//
+
+import UIKit
+
+class TravelTalkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
