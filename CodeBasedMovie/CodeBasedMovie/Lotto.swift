6f26bab2a6063823fc62d26d0b18c13d2b404108
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-14 17:20:51 +0900

    Feat: 로또 구조체, 로또 화면 UI, 네트워크 통신 구현

---
+++ CodeBasedMovie/CodeBasedMovie/Lotto.swift
@@ -0,0 +1,20 @@
+//
+//  Lotto.swift
+//  CodeBasedMovie
+//
+//  Created by Sebin Kwon on 1/14/25.
+//
+
+import Foundation
+
+struct Lotto: Decodable {
+    let drwNoDate: String
+    let drwNo: Int
+    let drwtNo1: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let drwtNo6: Int
+    let bnusNo: Int
+}
--- CodeBasedMovie/CodeBasedMovie/LottoViewController.swift
+++ CodeBasedMovie/CodeBasedMovie/LottoViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class LottoViewController: UIViewController {
     
@@ -32,6 +33,37 @@ class LottoViewController: UIViewController {
         label.font = .systemFont(ofSize: 13)
         return label
     }()
+    
+    let line: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray5
+        return view
+    }()
+    
+    let roundLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemYellow
+        label.font = .systemFont(ofSize: 25, weight: .bold)
+        return label
+    }()
+    
+    let resultLabel: UILabel = {
+        let label = UILabel()
+        label.text = "당첨결과"
+        label.font = .systemFont(ofSize: 25)
+        return label
+    }()
+    
+    let uiView = UIView()
+    
+    let ballStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .center
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,14 +72,101 @@ class LottoViewController: UIViewController {
         pickerView.delegate = self
         pickerView.dataSource = self
         configureTextField()
-        configureInfoLabel()
+        configureLabel()
+        getLottoData("1154")
+        configureBallStackView()
+        
     }
     
-    func configureInfoLabel() {
+    override func viewDidLayoutSubviews() {
+        print(ballStackView.frame.height)
+        ballStackView.subviews.forEach {
+            $0.layer.cornerRadius = ballStackView.frame.height / 2
+            guard let label = $0 as? UILabel else { return }
+            guard let text = label.text else { return }
+            guard let num = Int(text) else { return }
+            switch num {
+            case 1...10:
+                label.backgroundColor = .systemYellow
+            case 11...20:
+                label.backgroundColor = .systemBlue
+            case 21...30:
+                label.backgroundColor = .systemRed
+            case 31...40:
+                label.backgroundColor = .systemGray3
+            default:
+                label.backgroundColor = .systemGreen
+            }
+        }
+    }
+    
+    func configureBallUI() -> UILabel {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.textAlignment = .center
+        label.layer.masksToBounds = true
+        return label
+    }
+    
+    func configureBallStackView() {
+        view.addSubview(ballStackView)
+        for i in 0..<8 {
+            if i == 6 {
+                let plus: UILabel = {
+                    let label = UILabel()
+                    label.text = "+"
+                    label.textAlignment = .center
+                    label.font = .systemFont(ofSize: 15, weight: .bold)
+                    return label
+                }()
+                ballStackView.addArrangedSubview(plus)
+                continue
+            }
+            let ball = configureBallUI()
+            ball.text = String(i)
+            ball.snp.makeConstraints { make in
+                make.size.equalTo(40)
+            }
+            ballStackView.addArrangedSubview(ball)
+        }
+        ballStackView.snp.makeConstraints { make in
+            make.top.equalTo(uiView.snp.bottom).offset(60)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
+        }
+    }
+    
+    func configureLabel() {
         view.addSubview(infoLabel)
+        view.addSubview(dateLabel)
+        view.addSubview(line)
+        view.addSubview(uiView)
+        uiView.addSubview(roundLabel)
+        uiView.addSubview(resultLabel)
         infoLabel.snp.makeConstraints { make in
             make.top.equalTo(textField.snp.bottom).offset(30)
             make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
+        }
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).offset(30)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
+        }
+        line.snp.makeConstraints { make in
+            make.top.equalTo(infoLabel.snp.bottom).offset(10)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            make.height.equalTo(1)
+        }
+        uiView.snp.makeConstraints { make in
+            make.top.equalTo(line.snp.bottom).offset(30)
+            make.centerX.equalToSuperview()
+        }
+        roundLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(resultLabel.snp.leading).offset(-10)
+        }
+        resultLabel.snp.makeConstraints { make in
+            make.top.equalTo(line.snp.bottom).offset(30)
         }
     }
     
@@ -61,9 +180,26 @@ class LottoViewController: UIViewController {
         }
     }
     
-    func getLottoData() {
-        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1154"
-        
+    func getLottoData(_ num: String) {
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + num
+        AF.request(url, method: .get).responseDecodable(of: Lotto.self) { response in
+            switch response.result {
+            case .success(let value):
+                self.dateLabel.text = value.drwNoDate + " 추첨"
+                self.roundLabel.text = String(value.drwNo) + "회"
+                var ballList = self.ballStackView.subviews as? [UILabel]
+                guard let ballList else { return }
+                ballList[0].text = String(value.drwtNo1)
+                ballList[1].text = String(value.drwtNo2)
+                ballList[2].text = String(value.drwtNo3)
+                ballList[3].text = String(value.drwtNo4)
+                ballList[4].text = String(value.drwtNo5)
+                ballList[5].text = String(value.drwtNo6)
+                ballList[7].text = String(value.bnusNo)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 
@@ -71,13 +207,16 @@ class LottoViewController: UIViewController {
 
 extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        <#code#>
-//    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        getLottoData()
+    }
     
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        <#code#>
+    }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 10
+        return 1154
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
