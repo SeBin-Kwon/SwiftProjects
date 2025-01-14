a0c4490f4bdf9ad5ef520b9f43b7fbd4e53aee51
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-14 21:21:54 +0900

    Feat: 얼럿 익스텐션 추가

--- CodeBasedMovie/CodeBasedMovie/Extension/UIViewController+Extension.swift
+++ CodeBasedMovie/CodeBasedMovie/Extension/UIViewController+Extension.swift
@@ -5,4 +5,13 @@
 //  Created by Sebin Kwon on 1/14/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func displayAlert(_ title: String) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "확인", style: .cancel)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+}
